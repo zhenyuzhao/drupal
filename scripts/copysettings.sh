d2da94bb3c185660ca18bde440e1cfd3bfeb9304
Will St. Clair <wstclair@amazon.com>
2015-05-04 14:06:47 -0400

    Add ELB scripts

--- scripts/copysettings.sh
+++ scripts/copysettings.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 #
-# Navigate to the S3 console to find the name of the bucket for your
-# environment. (e.g.: devops-bootcamp-bucket-xxxxxxxxxxxx)
-#
-# Replace the value below with the name of the bucket from your
-# environment.
+# Navigate to the Outputs tab of the CloudFormation stack to find the
+# name of your environment's bucket. Replace the value below with the
+# name of the bucket from your environment.
 #
 BUCKETNAME=devops-bootcamp-bucket-1swxcabzjz1mh
 
 aws s3 cp s3://$BUCKETNAME/settings.php /var/www/html/sites/default/settings.php
 chown apache:apache /var/www/html/sites/default/settings.php
-chmod 444 /var/www/html/sites/default/settings.php
+chmod 400 /var/www/html/sites/default/settings.php
