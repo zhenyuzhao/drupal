41d41748793e718e5a6135828828d13300ebc09d
Will St. Clair <wstclair@amazon.com>
2015-05-06 15:58:17 -0400

    add New Relic

---
+++ scripts/install_new_relic.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -ex
+
+rpm -Uvh http://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarch.rpm
+yum install -y newrelic-php5
+
+export NR_INSTALL_SILENT=true
+export NR_INSTALL_KEY=your_new_relic_key_here
+newrelic-install install
