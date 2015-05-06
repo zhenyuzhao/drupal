2feb79a637caea1b4b8809c1614d8d38ded63e08
Will St. Clair <wstclair@amazon.com>
2015-05-06 16:07:57 -0400

    Fix return for reinstall

--- scripts/install_new_relic.sh
+++ scripts/install_new_relic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
-rpm -Uvh http://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarch.rpm
-yum install -y newrelic-php5
+rpm -Uvh http://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarch.rpm || true
+yum install -y newrelic-php5 || true
 
 export NR_INSTALL_SILENT=true
 export NR_INSTALL_KEY=dcbdfa1320afd5dec5c9f2aaee197036d9e22ace
