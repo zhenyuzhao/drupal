6292c475bcea91eae557a9f3b270a7c0d3bbe529
Will St. Clair <wstclair@amazon.com>
2015-05-06 16:03:14 -0400

    Actually add NR key

--- scripts/install_new_relic.sh
+++ scripts/install_new_relic.sh
@@ -4,5 +4,5 @@ rpm -Uvh http://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarc
 yum install -y newrelic-php5
 
 export NR_INSTALL_SILENT=true
-export NR_INSTALL_KEY=your_new_relic_key_here
+export NR_INSTALL_KEY=dcbdfa1320afd5dec5c9f2aaee197036d9e22ace
 newrelic-install install
