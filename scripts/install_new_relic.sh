dd040f60a4a0d8ecca4e9bb5b3a66879ce2eeeec
Zhenyu Zhao <zhenyu_zhao@harvard.edu>
2015-05-20 13:27:23 -0400

    changed

--- scripts/install_new_relic.sh
+++ scripts/install_new_relic.sh
@@ -4,5 +4,5 @@ rpm -Uvh http://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarc
 yum install -y newrelic-php5 || true
 
 export NR_INSTALL_SILENT=true
-export NR_INSTALL_KEY=your_new_relic_key_here
+export NR_INSTALL_KEY=f351ad6fe86c0f1cc49a88fde0fcfc9bc06ce90b
 newrelic-install install
